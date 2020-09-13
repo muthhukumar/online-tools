b24bea058515e77e1e18fdb51c851e22c87ab02d
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2020-09-13 23:05:47 +0530

    polyfill for unused css and cleanup

---
+++ components/stacktree/temp.js
@@ -0,0 +1,7 @@
+const Temp = () => {
+    return (
+        <div className="border-orange-500 border-blue-500 border-green-500 border-red-500 border-gray-500 border-teal-500 border-yellow-500 border-indigo-500 border-purple-500 border-pink-500" />
+    );
+};
+
+export default Temp;
